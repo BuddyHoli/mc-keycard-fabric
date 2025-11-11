8e2873b9348aaf97abe706c3a14685d426375a43
BuddyHoli <43436903+BuddyHoli@users.noreply.github.com>
2025-11-11 20:39:11 +0100

    Implement main mod class for KeyCard Mod

---
+++ src/main/java/com/buddyholi/mckeycard/MainModClass.java
@@ -0,0 +1,11 @@
+package com.buddyholi.mckeycard;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MainModClass implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("KeyCard Mod loaded!");
+        // TODO: Register commands, event listeners, etc.
+    }
+}
